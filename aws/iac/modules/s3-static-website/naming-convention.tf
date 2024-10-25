0a76f56056532d1146453cf1a61bd478dfa3454c
Felipe Augusto <felipe.augusto@iteris.com.br>
2024-10-25 16:42:45 -0300

    update naming-convention.tf

--- aws/iac/modules/s3-static-website/naming-convention.tf
+++ aws/iac/modules/s3-static-website/naming-convention.tf
@@ -1,6 +1,7 @@
 module "this" {
-  source         = "git@gitlab.anbima.com.br:anbima/cloud/aws/iac/modules/naming-convention.git?ref=main"
+  source         = "git::ssh://git@github.com/51augusto/Terraform.git//aws/iac/modules/naming-convention?ref=master"
   component_name = var.component_name
   environment    = var.environment
   region         = var.region
 }
+
